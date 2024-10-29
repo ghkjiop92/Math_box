5097c4629c24235c8a79c993945a589f8eea1dc0
강동우 <gangdong-u@gangdong-uui-MacBookAir.local>
2024-10-29 20:27:23 +0900

    Save progress on calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# 기본 계산기
+def add(a,b):
+        return a+b
+
+
+def subtrack(a,b):
+        return a-b
+                                                
+def  multiply(a,b):
+        return a*b
+                                
+
+def  multiply(a,b):
+        return a*b
+                                        
+
+def  divide_free(a,b):
+        return a/b
+                                        
